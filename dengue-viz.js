bbbc01480ce26b1165f5974571b7c66e6596dc61
Wong Jiang Fung <kakarukeys@gmail.com>
2014-08-10 14:28:52 +0800

    adjusted map size disabled zooming by mouse wheel set the correct view used html5 boilerplate added markercluster plugin

--- dengue-viz.js
+++ dengue-viz.js
@@ -1,14 +1,16 @@
-var map = L.map('map').setView([1.31254, 103.84305], 15),
+var map = L.map('map', {
+        scrollWheelZoom: false
+    }).setView([1.347833, 103.809357], 12),
     data = [
-      {coords: [1.32, 103.847], name: "Tan Tock Seng Hospital"},
-      {coords: [1.305, 103.845], name: "Istana Park"}
+        {coords: [1.32, 103.847], name: "Tan Tock Seng Hospital"},
+        {coords: [1.305, 103.845], name: "Istana Park"}
     ];
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/kakarukeys.iofn8meo/{z}/{x}/{y}.png', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
 }).addTo(map);
 
 _.each(data, function(obj) {
-  L.marker(obj.coords).addTo(map);
+    L.marker(obj.coords).addTo(map);
 });
