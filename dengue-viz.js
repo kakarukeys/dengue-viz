37dbb9e99d961a0d4a6327185e11145636db7831
Wong Jiang Fung <kakarukeys@gmail.com>
2014-08-10 15:20:40 +0800

    added clusters refactoring enabled zooming by mouse wheel again load data by getJSON

--- dengue-viz.js
+++ dengue-viz.js
@@ -1,16 +1,29 @@
-var map = L.map('map', {
-        scrollWheelZoom: false
-    }).setView([1.347833, 103.809357], 12),
-    data = [
-        {coords: [1.32, 103.847], name: "Tan Tock Seng Hospital"},
-        {coords: [1.305, 103.845], name: "Istana Park"}
-    ];
+function create_map() {
+    var map = L.map('map').setView([1.347833, 103.809357], 12);
 
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/kakarukeys.iofn8meo/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
-}).addTo(map);
+    L.tileLayer('http://{s}.tiles.mapbox.com/v3/kakarukeys.iofn8meo/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18
+    }).addTo(map);
 
-_.each(data, function(obj) {
-    L.marker(obj.coords).addTo(map);
+    return map;
+}
+
+function add_clusters(map, markers) {
+    var clusters = L.markerClusterGroup();
+    clusters.addLayers(markers);
+    map.addLayer(clusters);    
+}
+
+function create_marker(d) {
+    return L.marker(d.coords, {title: d.name});
+}
+
+var map = create_map();
+
+$.getJSON("cluster_data.json", function(data) {
+    var cases = data.snapshots[0].cases,
+        markers = _.map(cases, create_marker);
+
+    add_clusters(map, markers);
 });
