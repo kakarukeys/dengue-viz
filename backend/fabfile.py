cb57a50c26c821f645b8ecf94caa65d1580a0417
Wong Jiang Fung <kakarukeys@gmail.com>
2014-09-15 23:39:11 +0800

    corrected spacing problem

--- backend/fabfile.py
+++ backend/fabfile.py
@@ -6,23 +6,23 @@ from fabric.contrib.files import exists
 
 
 def deploy():
-	with open("deployment_settings.json") as f:
-		deployment_settings = json.load(f)
+    with open("deployment_settings.json") as f:
+        deployment_settings = json.load(f)
 
-	env.host_string = deployment_settings["host_string"]
+    env.host_string = deployment_settings["host_string"]
 
-	base_dir = deployment_settings["base_dir"]
-	file_dir = posixpath.join(base_dir, "dengue-viz")
-	
-	if exists(file_dir):
-		print("Updating the files to latest commit in {0}".format(file_dir))
-		with cd(file_dir):
-			run("git pull origin master")
-	else:
-		print("Deploying for the first time!")
-		print("Cloning the git repository in {0}".format(base_dir))
-		with cd(base_dir):
-			run("git clone https://github.com/kakarukeys/dengue-viz.git")
+    base_dir = deployment_settings["base_dir"]
+    file_dir = posixpath.join(base_dir, "dengue-viz")
+    
+    if exists(file_dir):
+        print("Updating the files to latest commit in {0}".format(file_dir))
+        with cd(file_dir):
+            run("git pull origin master")
+    else:
+        print("Deploying for the first time!")
+        print("Cloning the git repository in {0}".format(base_dir))
+        with cd(base_dir):
+            run("git clone https://github.com/kakarukeys/dengue-viz.git")
         
         link_dir = deployment_settings["link_dir"]
         webapp_dir = posixpath.join(file_dir, "webapp")
